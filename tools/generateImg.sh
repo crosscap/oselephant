7e29b07032d018ee90440e74be35b300a9851a79
crosscap <wangliankun2018@outlook.com>
2025-01-21 20:06:46 +0800

    Add testing in generateImg tool

--- tools/generateImg.sh
+++ tools/generateImg.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
-bin/bximage -hd -mode="flat" -size=60 -q hd60M.img
-nasm -I src/include/ -o src/mbr.bin src/mbr.S
+if [[ ! -f ./src/hd60M.img ]]; then
+  bin/bximage -hd -mode="flat" -size=60 -q hd60M.img
+
+fi
+
+if [[ ! -f ./src/mbr.bin ]]; then
+  nasm -I src/include/ -o src/mbr.bin src/mbr.S
+fi
+
+if [[ ! -f ./src/loader.bin ]]; then
+  nasm -I src/include/ -o src/loader.bin src/loader.S
+fi
+
+if [[ ! -f ./src/kernel/kernel.bin ]]; then
+  gcc -m32 -c -o src/kernel/main.o src/kernel/main.c
+  ld src/kernel/main.o -m elf_i386 -Ttext 0xc0001500 -e main -o src/kernel/kernel.bin
+fi
+
 dd if=./src/mbr.bin of=./hd60M.img bs=512 count=1 conv=notrunc
-nasm -I src/include/ -o src/loader.bin src/loader.S
 dd if=./src/loader.bin of=./hd60M.img bs=512 count=4 seek=2 conv=notrunc
-gcc -m32 -c -o src/kernel/main.o src/kernel/main.c
-ld src/kernel/main.o -m elf_i386 -Ttext 0xc0001500 -e main -o src/kernel/kernel.bin
 dd if=./src/kernel/kernel.bin of=./hd60M.img bs=512 count=200 seek=9 conv=notrunc
